72d4951bb51137594aafeeafc672ea5f0c1c4865
Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>
2023-04-11 14:00:29 +0200

    Fmt
    Signed-off-by: Oliver Tale-Yazdi <oliver.tale-yazdi@parity.io>

--- src/test.rs
+++ src/test.rs
@@ -12,11 +12,11 @@ use super::*;
 #[test]
 fn example_works() {
 	let warning = Warning::new_deprecated("my_macro")
-	    .old("my_macro()")
-	    .new("my_macro::new()")
-	    .help_link("https:://example.com")
-	    .span(proc_macro2::Span::call_site())
-	    .build();
+		.old("my_macro()")
+		.new("my_macro::new()")
+		.help_link("https:://example.com")
+		.span(proc_macro2::Span::call_site())
+		.build();
 	let got = quote!(#warning);
 
 	let want = quote!(
@@ -27,10 +27,11 @@ fn example_works() {
 		#[allow(non_camel_case_types)]
 		#[allow(non_snake_case)]
 		fn my_macro() {
-			#[deprecated(note = "\n\t\tIt is deprecated to my_macro().\n\t\tPlease instead my_macro::new().\n\n\t\tFor more info see:\n\t\t\t<https:://example.com>")]
-			
+			#[deprecated(
+				note = "\n\t\tIt is deprecated to my_macro().\n\t\tPlease instead my_macro::new().\n\n\t\tFor more info see:\n\t\t\t<https:://example.com>"
+			)]
 			#[allow(non_upper_case_globals)]
-			const _w : () = ();
+			const _w: () = ();
 			let _ = _w;
 		}
 	);
